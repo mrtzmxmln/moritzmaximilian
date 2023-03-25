63b1d805c8ff0a43d98adb3b95f2c76d425ef5e8
Moritz <moritzmaximilian@gmx.net>
2023-03-25 13:42:11 +0100

    .

---
+++ script/redirects.js
@@ -0,0 +1,11 @@
+function openInstagram() {
+    window.open('https://www.instagram.com/mrtzmxmln', '_blank');
+}
+
+function openGitHub() {
+    window.open('https://github.com/mrtzmxmln', '_blank');
+}
+
+function openLinkedIn() {
+    window.open('https://linkedin.com/in/moritzmaximilian', '_blank');
+}
